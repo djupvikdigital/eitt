14958ddc273f28c2c80ca40cdf77a380c1a5c13c
Reidar Djupvik <reidar@djupvik.digital>
2020-09-26 19:12:33 +0100

    Generate 7 cards

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+import { generateCard } from '/client/game.js';
+
 function createClickHandler(card) {
     return function clickHandler() {
         socket.emit('playCard', card);
@@ -7,7 +9,7 @@ function createClickHandler(card) {
 function renderCards(cards) {
     let cardsElement = document.getElementById('cards');
     let fragment = document.createDocumentFragment();
-    for (card of cards) {
+    for (let card of cards) {
         let element = document.createElement('button');
         element.addEventListener('click', createClickHandler(card));
         element.textContent = JSON.stringify(card);
@@ -16,7 +18,12 @@ function renderCards(cards) {
     cardsElement.appendChild(fragment);
 }
 
-let cards = [{ color: 'red', value: 'S' }, { color: 'green', value: '+2' }];
+let cards = [];
+for (let i = 0; i <= 7; i++) {
+    cards.push(generateCard());
+}
+
+console.log(cards);
 
 renderCards(cards);
 
