847319b5e5542496b636042f91e9df31b0ad8b6a
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2020-09-26 18:17:06 +0100

    added lastPlayedCard

--- app.js
+++ app.js
@@ -14,14 +14,27 @@ console.log('Server started.');
 
 let SOCKET_LIST = {};
 
+let game = require('./game.mjs')
+
+let lastPlayedCard = game.generateCard();
+
 let io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
     console.log('socket connection');
+    for(var i in SOCKET_LIST){
+        var currentSocket = SOCKET_LIST[i];
+        currentSocket.emit('lastPlayed',data);
+    }
 
     socket.on('playCard',function(data){
-        console.log("Yay! Someone played a " + data.color + data.value)
+        console.log("Yay! Someone played a " + data.color + " " + data.value);
+        for(var i in SOCKET_LIST){
+            var currentSocket = SOCKET_LIST[i];
+            currentSocket.emit('lastPlayed',data);
+        }
+        lastPlayedCard = data;
     });
 
     socket.on('disconnect',function(){
