29f76e881fb2fb3ac2b6eb343bfd86c00cf10184
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2020-12-24 14:39:39 +0000

    added random name generation

--- app.js
+++ app.js
@@ -104,6 +104,13 @@ function sendGameStatus(){
     }
 }
 
+let playerFName = ['attractive', 'bald', 'beautiful', 'chubby', 'clean', 'dazzling', 'drab', 'elegant', 'fancy', 'fit', 'flabby', 'glamorous', 'gorgeous', 'handsome', 'long', 'magnificent', 'muscular', 'plain', 'plump', 'quaint', 'scruffy', 'shapely', 'short', 'skinny', 'stocky', 'ugly', 'unkempt', 'unsightly']
+let playerBName = ["people","history","way","art","world","information","map","family","government","health","system","computer","meat","year","thanks","music","person","reading","method","data","food","understanding","theory","law","bird","literature","problem","software","control","knowledge","power","ability","economics","love","internet","television","science","library","nature","fact","product","idea","temperature","investment","area","society","activity","story","industry","media","thing","oven","community","definition","safety","quality","development","language","management","player","variety","video","week","security","country","exam","movie","organization","equipment","physics","analysis","policy","series","thought","basis","boyfriend","direction","strategy","technology","army","camera","freedom","paper","environment","child","instance","month","truth","marketing","university","writing","article","department","difference","goal","news","audience","fishing","growth","income","marriage","user","combination","failure","meaning","medicine","philosophy","teacher","communication","night","chemistry","disease","disk","energy","nation","road","role","soup","advertising","location","success","addition","apartment","education","math","moment","painting","politics","attention","decision","event","property","shopping","student","wood","competition","distribution","entertainment","office","population","president"]
+
+function pickRand(array) {
+    let rand = array[(Math.random() * array.length) | 0]
+    return rand
+}
 
 let io = socketio(serv,{});
 io.sockets.on('connection', function(socket){
@@ -113,7 +120,11 @@ io.sockets.on('connection', function(socket){
     }
     socket.cards = cards;
     socket.id = Math.random();
-    socket.name = "Unnamed";
+
+    let bName = pickRand(playerBName)
+    bName = bName.charAt(0).toUpperCase() + bName.slice(1)
+    socket.name = pickRand(playerFName) + bName
+
     SOCKET_LIST[socket.id] = socket;
     console.log('socket connection');
     turnAssign();
