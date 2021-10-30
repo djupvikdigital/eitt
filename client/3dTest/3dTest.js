4afafd8cda2f2fd69bd8d864ee89dad610eea279
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2021-10-30 21:44:45 +0100

    half working 3d halløj

--- client/3dTest/3dTest.js
+++ client/3dTest/3dTest.js
@@ -1,38 +1,13 @@
-function Table() {
-    const table = new THREE.Group()
-
-    const tablePlate = new THREE.Mesh(
-        new THREE.CircleBufferGeometry(160, 50),
-        new THREE.MeshLambertMaterial({ color: 0x333333})
-    )
-
-    tablePlate.rotation.x-=1.5
-
-    table.add(tablePlate)
-
-    return table
-}
-
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffffff );
 
-const table = Table()
+const table = ModelTable()
 scene.add(table)
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
-scene.add(ambientLight)
-
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
-dirLight.position.set(100, -300, 400)
-scene.add(dirLight)
-
-
+const card = ModelCard('Green', '8')
+scene.add(card)
 
 const aspectRatio = window.innerWidth / window.innerHeight
-
-const cameraWidth = 500
-const cameraHeight = cameraWidth / aspectRatio
-
 
 const camera = new THREE.PerspectiveCamera(
     30,
@@ -41,20 +16,34 @@ const camera = new THREE.PerspectiveCamera(
     2000
 )
 
-/*
-const camera = new THREE.OrthographicCamera(
-    cameraWidth / -2,
-    cameraWidth / 2,
-    cameraHeight / 2,
-    cameraHeight / -2,
-    0,
-    1000
-)
-*/
+let numPlayers = 5
+let circleLength = 200
 
-camera.position.set(200, 100, 200)
-//camera.up.set(0, 0, 1)
+function calcPointInCircle(numPlayers, circleLength) {
+    for (let i = 0; i < numPlayers; i++) {
+        let dir = 2 * Math.PI / numPlayers * i
+        let tempX = Math.cos(dir) * circleLength
+        let tempY = Math.sin(dir) * circleLength
+        let own = false
+        if (i == 0) own = true
+        const player = ModelPlayer(own)
+        player.position.set(tempX, 0, tempY)
+        player.rotation.y = 2 * Math.PI - dir
+        scene.add(player)
+        if (i == 0) camera.position.set(tempX, 75, tempY)
+    }
+}
+
+calcPointInCircle(numPlayers, circleLength)
+
 camera.lookAt(0, 0, 0)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+scene.add(ambientLight)
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
+dirLight.position.set(100, -300, 400)
+scene.add(dirLight)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true})
 renderer.setSize(window.innerWidth -20, window.innerHeight -20)
@@ -69,8 +58,16 @@ window.addEventListener( 'click', function () {
 
 } );
 
+console.log(myCards)
+
+// -1.04 -0.26 0.52 1.30
 
 setInterval(function(){
+    let worldDir = new THREE.Vector3
+    camera.rotation.toVector3(worldDir)
+    if (worldDir.y < 1) console.log(Math.PI - worldDir.z - Math.PI / 2)
+
     renderer.render(scene, camera)
+    
 },1000/50)
 
