59b1a676add9623650e7042f6296ee86d0d2a690
Reidar Djupvik <reidar@djupvik.digital>
2022-01-07 23:17:24 +0000

    Only show Pass and Check +4 when needed

--- client/room.js
+++ client/room.js
@@ -213,6 +213,8 @@ function setGameStatus(status) {
         changeButtonDisableState(false)
         document.getElementById('game-options').style.display = ''
         document.getElementById('game-table').style.display = 'block'
+        document.getElementById('pass').style.display = status.hasDrawn ? 'inline-block' : ''
+        document.getElementById('check-plus-four').style.display = status.plusFourInPlay && status.hasTurn ? 'inline-block' : ''
         document.getElementById('round-controls').style.visibility = ''
     }
     else if (status.state === 'ROUND_FINISHED') {
