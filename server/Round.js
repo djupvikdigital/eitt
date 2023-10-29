98e83a8285c2eb9e6026feefb7ac931630170c6b
Reidar Djupvik <reidar@djupvik.digital>
2023-10-29 22:40:12 +0000

    Let round control turn

--- server/Round.js
+++ server/Round.js
@@ -1,11 +1,34 @@
-import { CardDeck } from "./CardDeck"
+import { CardDeck } from './CardDeck'
+import { Turn } from './Turn'
 
 export function Round() {
     let self = {
-        deck: CardDeck()
+        deck: CardDeck(),
+        plusFourInPlay: false,
+        turn: Turn()
     }
-    self.playTurn = function (turn) {
-        return this.deck.playCards(turn.cardsToPlay)
+    self.playTurn = function () {
+        let turn = this.turn
+        if (this.plusFourInPlay) {
+            return false
+        }
+        let gotPlayed = false
+        for (let i= 0; i < turn.cardsToPlay.length; i++) {
+            let card = turn.cardsToPlay[i]
+            switch(card.value) {
+                case '+4':
+                    this.plusFourInPlay = true
+                    break;
+                default:
+                    break;
+            }
+            gotPlayed = this.deck.playCard(card)
+            if (!gotPlayed) {
+                return false
+            }
+        }
+        this.turn = Turn()
+        return gotPlayed
     }
     return self
 }
--- server/Round.test.js
+++ server/Round.test.js
@@ -1,15 +1,26 @@
+import { CardDeck } from './CardDeck'
 import { Round } from './Round'
 import { Turn } from './Turn'
 
 describe('Round', () => {
     it('can play a turn', () => {
-        let turn = Turn()
         let card = { color: 'blue', value: 1 }
         let round = Round()
-        turn.addCardToPlay(card)
-        round.playTurn(turn)
+        round.turn.addCardToPlay(card)
+        round.playTurn()
         let playedCard = round.deck.playedCards[round.deck.playedCards.length - 1]
-        expect(playedCard).toEqual(turn.cardsToPlay[turn.cardsToPlay.length - 1])
         expect(playedCard).toEqual(card)
+    })
+
+    it('playing +4 makes playing another card impossible', () => {
+        let card = { color: 'black', value: '+4' }
+        let round = Round()
+        round.turn.addCardToPlay(card)
+        let played = round.playTurn()
+        expect(played).toBe(true)
+        card = { color: 'green', value: 1 }
+        round.turn.addCardToPlay(card)
+        played = round.playTurn()
+        expect(played).toBe(false)
     })
 })
