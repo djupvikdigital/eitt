43a2d20d1745e0cd7386b6a63a80162c2630def0
Reidar Djupvik <reidar@djupvik.digital>
2023-11-17 15:30:13 +0000

    Remove moved Turn methods

--- server/Turn.js
+++ server/Turn.js
@@ -7,26 +7,5 @@ export function Turn(playerIndex, playMultiple = false) {
         plusFourInPlay: false,
         plusTwoInPlay: 0
     }
-    self.addCardToPlay = function (player, index) {
-        if (!playMultiple && this.cardsToPlay.length) {
-            return false
-        }
-        let card = player.cards[index]
-        // disallow adding another card with different value
-        if (this.cardsToPlay.length && card.value !== player.cards[this.cardsToPlay[0]].value) {
-            return false
-        }
-        if (this.plusFourInPlay) {
-            return false
-        }
-        if (this.plusTwoInPlay && card.value !== '+2') {
-            return false
-        }
-        this.cardsToPlay.push(index)
-        return true
-    }
-    self.removeCardFromPlay = function (index) {
-        this.cardsToPlay.splice(index, 1)
-    }
     return self
 }
