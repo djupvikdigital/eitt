a94370ea5bb96e87a11b0e1b8edd1cdb253628b0
Reidar Djupvik <reidar@djupvik.digital>
2023-12-29 14:08:32 +0000

    Set lastPlayerIndex from previousTurn

--- server/GameControler.js
+++ server/GameControler.js
@@ -282,12 +282,9 @@ export function GameControler(name) {
             self.addScoresForRound()
         }
         let pack = [];
-        let lastPlayerIndex = -1;
+        let lastPlayerIndex = this.round.previousTurn ? this.round.previousTurn.playerIndex : -1;
         for (let i = 0; i < this.players.length; i++) {
             let currentPlayer = this.players[i]
-            if (currentPlayer.id === this.lastPlayerId) {
-                lastPlayerIndex = i;
-            }
             pack.push({
                 name: currentPlayer.name,
                 numberOfCards: currentPlayer.cards.length,
