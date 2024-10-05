a3c3a2824ec3c59032a089f5befd803c6eed03f0
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2024-10-05 20:28:26 +0100

    Merge conflicts

--- client/modules/handlers.js
+++ client/modules/handlers.js
@@ -7,19 +7,29 @@ export function createTransitionEndHandler(i) {
 
 function showColorPicker() {
     document.getElementById('color-picker').style.display = 'inline';
+    document.getElementById('play').style.display = 'none';
 }
 
-function addCardToPlay(index, color, socket) {
+function hideColorPicker() {
+    document.getElementById('color-picker').style.display = 'none';
+} 
+
+function addCardToPlay(index, socket) {
     let data = { index: index };
-    if (color) {
-        data.color = color;
-    }
     socket.emit('addCardToPlay', data);
 }
 
 function removeCardFromPlay(index, socket) {
     let data = { index: index };
     socket.emit('removeCardFromPlay', data);
+}
+
+function playTurn(color, socket) {
+    let data = {}
+    if (color) {
+        data.color = color;
+    }
+    socket.emit('playTurn', data);
 }
 
 export function createClickHandler(i, card, toPlay, currentIndex, socket) {
@@ -29,12 +39,13 @@ export function createClickHandler(i, card, toPlay, currentIndex, socket) {
             showColorPicker();
             event.stopPropagation()
         }
-        else if (toPlay) {
+        if (toPlay) {
             removeCardFromPlay(i, socket)
+            hideColorPicker()
             console.log('removeCardFromPlay emitted, index = ' + i)
         }
         else {
-            addCardToPlay(i, null, socket)
+            addCardToPlay(i, socket)
             console.log('addCardToPlay emitted, index = ' + i)
         }
     }
@@ -42,18 +53,18 @@ export function createClickHandler(i, card, toPlay, currentIndex, socket) {
 
 export function createPickers(socket) {
     document.getElementById('pick-blue').addEventListener('click', function () {
-        addCardToPlay(currentIndex, 'blue', socket);
+        playTurn('blue', socket);
     });
     
     document.getElementById('pick-green').addEventListener('click', function () {
-        addCardToPlay(currentIndex, 'green', socket);
+        playTurn('green', socket);
     });
     
     document.getElementById('pick-red').addEventListener('click', function () {
-        addCardToPlay(currentIndex, 'red', socket);
+        playTurn('red', socket);
     });
     
     document.getElementById('pick-yellow').addEventListener('click', function () {
-        addCardToPlay(currentIndex, 'yellow', socket);
+        playTurn('yellow', socket);
     });
 }
